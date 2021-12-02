13d577d71e642e612409eab95abb854e31c7de16
Wave <reduts@gmail.com>
2021-12-02 15:58:16 +0100

    premake update

--- premake5.lua
+++ premake5.lua
@@ -2,21 +2,23 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%(prj.name)")
-	objdir ("bin-intermediate/" .. outputdir .. "/%(prj.name)")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
 		"imconfig.h",
-		"imgui.h",
 		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
-		"imgui_widgets.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+
 	}
 
 	filter "system:windows"
